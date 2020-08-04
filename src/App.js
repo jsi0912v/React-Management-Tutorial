8c721a75b064aa7a93a656633bdfa3194511111a
jsi0912v <jsi0912v@gamil.com>
2020-08-04 22:45:14 +0900

    Apply material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer'
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [
   {
@@ -28,10 +48,24 @@ const customers = [
       'job' : 'pet'
   }
 ]
-function App() {
+class App extends Component {
+  render(){
+    const { classes } = this.props;
   return (
-    <div>
-      {
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {
         //map은 key값이 필수, 배열에 접근하여 출력.
         customers.map(c => {
           return <Customer
@@ -45,8 +79,12 @@ function App() {
           />
         })
       }
-      </div>
+        </TableBody>
+      </Table>
+      
+      </Paper>
   );
 }
+}
 
-export default App;
+export default withStyles (styles)(App);
