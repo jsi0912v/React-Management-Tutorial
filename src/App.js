0582dc308dd71960d0d60430d74e56496d789332
jsi0912v <jsi0912v@gamil.com>
2020-08-04 22:02:30 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
 
+const customers = [
+  {
+  'id': 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name': 'Jeon',
+  'birthday': '870912',
+  'gender' : 'male',
+  'job' : 'worker'
+  },
+  {
+    'id': 2,
+    'image' : 'https://placeimg.com/64/64/2',
+    'name': 'Ko',
+    'birthday': '880225',
+    'gender' : 'female',
+    'job' : 'owner'
+   },
+   {
+      'id': 3,
+      'image' : 'https://placeimg.com/64/64/3',
+      'name': 'Koo',
+      'birthday': '161110',
+      'gender' : 'male',
+      'job' : 'pet'
+  }
+]
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
+    <div>
+      {
+        //map은 key값이 필수, 배열에 접근하여 출력.
+        customers.map(c => {
+          return <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender = {c.gender}
+            job={c.job}
+          />
+        })
+      }
+      </div>
   );
 }
 
