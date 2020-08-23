efcec19693ac1cce161e379d08b4fcc3a81292b1
jsi0912v <jsi0912v@gamil.com>
2020-08-23 16:23:55 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,38 +1,40 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
+//file을 읽어 온다.
+const data = fs.readFileSync('./database.json');
+
+//읽어온 파일을 파싱하여 정보를 얻는다.
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+
+//DB 접속정보 저장.
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+
+//data 베이스에 접속
+connection.connect();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/api/customers', (req, res) => {
-    res.send([
-            {
-            'id': 1,
-            'image' : 'https://placeimg.com/64/64/1',
-            'name': 'Jeon',
-            'birthday': '870912',
-            'gender' : 'male',
-            'job' : 'worker'
-            },
-            {
-              'id': 2,
-              'image' : 'https://placeimg.com/64/64/2',
-              'name': 'Ko',
-              'birthday': '880225',
-              'gender' : 'female',
-              'job' : 'owner'
-             },
-             {
-                'id': 3,
-                'image' : 'https://placeimg.com/64/64/3',
-                'name': 'Koo',
-                'birthday': '161110',
-                'gender' : 'male',
-                'job' : 'pet'
-            }
-          ]);
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err, rows, fields) => {
+        console.log(err);
+        res.send(rows);
+      }
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
